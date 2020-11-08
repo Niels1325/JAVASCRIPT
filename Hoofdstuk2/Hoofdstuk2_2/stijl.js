9a5a7f5e76044e64c1c6031b784472ddd523d661
NeLLo <Nkloosterman@roc-dev.com>
2020-11-08 14:36:45 +0100

    Hoofdstuk 2 af

---
+++ Hoofdstuk2/Hoofdstuk2_2/stijl.js
@@ -0,0 +1,12 @@
+document.getElementById("bl1").style.backgroundColor = "cyan"
+document.getElementById("bl1").style.paddingTop = "15px"
+document.getElementById("bl1").style.textAlign = "center"
+
+document.getElementById("bl2").style.backgroundColor = "green"
+document.getElementById("bl2").style.marginTop = "15px"
+document.getElementById("bl2").style.textAlign = "center"
+
+document.getElementById("bl3").style.marginTop = "40px"
+document.getElementById("bl3").style.textAlign = "right"
+
+document.getElementById("bl4").style.backgroundColor = "red"
