c706da3106bc1be63097c15c31a0315a9b55b4f8
NeLLo <Nkloosterman@roc-dev.com>
2020-11-13 19:02:09 +0100

    Almost fixed

--- Hoofdstuk6/memorygame.js
+++ Hoofdstuk6/memorygame.js
@@ -1,36 +1,123 @@
-var player1 = document.getElementById("player1");
-var player2 = document.getElementById("player2");
-var player1points = document.getElementById("player1points");
-var player2points = document.getElementById("player2points");
+const cards = document.querySelectorAll('.memory-card');
 
-var cardBack1 = document.getElementById("cards1");
-var cardBack2 = document.getElementById("cards2");
+let hasFlippedCard = false;
+let lockBoard = false;
+let firstCard, secondCard;
+let player1 = document.getElementById('player1points');
+let player1pointscore = 0;
+let player2 = document.getElementById('player2points');
+let player2pointscore = 0;
 
+var turnCount = 0;
+var winner = null;
+var playerturn = null;
+var thewinner = document.getElementById("thewinner");
 var turn = document.getElementById("turn");
-var card = document.getElementById("box");
-turnCard();
 
-var random = 0;
-var randomNumbers = [];
+function flipCard() {
+    if (lockBoard) return;
+    if (this === firstCard) return;
 
-while(randomNumbers.length < 9) {
-    random = Math.floor(Math.random() * 9) + 1;
-    if(randomNumbers.lastIndexOf(random) === -1) {
-        randomNumbers.push(random);
+    this.classList.add('flip');
+
+    if (!hasFlippedCard) {
+        hasFlippedCard = true;
+        firstCard = this;
+
+        return;
     }
+
+    secondCard = this;
+    checkForMatch();
 }
 
-function turnCard() {
-    card.addEventListener("click", function (){
-        cardBack2.style.visibility = 'invisible';
-        cardBack1.style.visibility = 'invisible';
-        card.style.backgroundImage = "url('img/monkey"+ random + ".jpg')";
-        card.style.backgroundRepeat = "no-repeat";
-        console.log(turnCard());
+function checkForMatch() {
+    let isMatch
+    if ( isMatch = firstCard.dataset.framework === secondCard.dataset.framework) {
+        playerscore();
+    }
+
+    isMatch ? disableCards() : unflipCards();
+}
+
+function disableCards() {
+    firstCard.removeEventListener('click', flipCard);
+    secondCard.removeEventListener('click', flipCard);
+
+    resetBoard();
+}
+
+function unflipCards() {
+    lockBoard = true;
+
+    setTimeout(() => {
+        firstCard.classList.remove('flip');
+        secondCard.classList.remove('flip');
+
+        resetBoard();
+    }, 500);
+
+    turn();
+}
+
+function resetBoard() {
+    [hasFlippedCard, lockBoard] = [false, false];
+    [firstCard, secondCard] = [null, null];
+}
+
+(function shuffle() {
+    cards.forEach(card => {
+        let randomPos = Math.floor(Math.random() * 12);
+        card.style.order = randomPos;
     });
+})();
+
+function turn() {
+    turnCount++
+    if (turnCount % 2) {
+        playerturn = "Player 2 is aan de beurt";
+    }
+    else {
+        playerturn = "Player 2 is aan de beurt";
+    }
+    turn.innerHTML = playerturn;
 }
 
+function playerscore() {
+    if (turnCount % 2) {
+        player2pointscore +=1;
+        player2.innerHTML = player2pointscore;
+    }
+    else  {
+        player1pointscore +=1;
+        player1.innerHTML = player1pointscore;
+    }
+    endgame();
+}
 
+function endgame() {
+    if (player1pointscore + player2pointscore < 10) {
 
+    } else {
+        whowon();
+    }
 
+}
 
+function whowon() {
+    if (player1pointscore > player2pointscore) {
+        winner = player1 + " won the game!";
+    }
+    else if ((player1pointscore < player2pointscore)){
+        winner = player2 + " won the game!";
+    }
+    else {
+        winner = "Tied game";
+    }
+    console.log(winner);
+    thewinner.innerHTML = winner;
+    thewinner.style.visibility = 'visible'
+    turn.style.visibility = 'hidden'
+}
+
+cards.forEach(card => card.addEventListener('click', flipCard));
